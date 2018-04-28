eaae65d2c8d5ae7a0cb2fb74b6d594274fa3d806
DravitLochan <dlochangupta@gmail.com>
2018-04-28 22:08:03 +0530

    fetched complete papers table from html

--- GoogleScholarScraper.py
+++ GoogleScholarScraper.py
@@ -1,15 +1,19 @@
+# for each paper, store top 5 authors, citation, year
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 url = "https://scholar.google.co.in/citations?view_op=search_authors&mauthors=anuja+arora+jiit&hl=en&oi=ao"
 base_link = "https://scholar.google.co.in"
+papersInfo = []
+papersname = set([])
 
 def main():
         file = open('temp.html', 'w', newline = '')
         page = urlopen(url)
         soup = BeautifulSoup(page)
         all_anchors = soup.find_all('a')
-        print(soup)
+        # print(soup)
         print(all_anchors.__len__())
         # for anchor in all_anchors:
             # print(anchor)
@@ -22,8 +26,20 @@ def main():
         profile_page = urlopen(profile_link + "")
         profile_soup = BeautifulSoup(profile_page)
         print("\n")
-        print(profile_soup)
-        file.write(str(profile_soup))
+        # print(profile_soup)
+        # file.write(str(profile_soup))
+
+        # from this point, work flow is as follows :
+        # pick a paper. if it does not exists in current data (papersname), create a new papersInfo.
+        # else add an additional column in the papersInfo
+        allTables = profile_soup.find_all('table')
+        dataTable = allTables[1]
+
+        # print(dataTable)
+        print(type(dataTable))
+        body = dataTable.find_all('body')
+        print(body)
+
 
 
 if __name__ == "__main__":
