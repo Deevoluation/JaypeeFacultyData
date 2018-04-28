01f344789342bd92995098b2b074355884f2337b
DravitLochan <dlochangupta@gmail.com>
2018-04-29 04:27:18 +0530

    fetched all details for a single paper

--- GoogleScholarScraper.py
+++ GoogleScholarScraper.py
@@ -1,46 +1,43 @@
 # for each paper, store top 5 authors, citation, year
 
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 url = "https://scholar.google.co.in/citations?view_op=search_authors&mauthors=anuja+arora+jiit&hl=en&oi=ao"
 base_link = "https://scholar.google.co.in"
 papersInfo = []
 papersname = set([])
 
+
 def main():
-        file = open('temp.html', 'w', newline = '')
-        page = urlopen(url)
-        soup = BeautifulSoup(page)
-        all_anchors = soup.find_all('a')
-        # print(soup)
-        print(all_anchors.__len__())
-        # for anchor in all_anchors:
-            # print(anchor)
-        # temp = soup.select_one("a[href*=/citations?user]")
-        # print(temp)
-        # print(temp['href'])
-        faculty_link = soup.select_one("a[href*=/citations?user]")
-        profile_link = base_link + faculty_link['href']
-        print(profile_link)
-        profile_page = urlopen(profile_link + "")
-        profile_soup = BeautifulSoup(profile_page)
-        print("\n")
-        # print(profile_soup)
-        # file.write(str(profile_soup))
+    file = open('temp.html', 'w', newline='')
+    page = urlopen(url)
+    soup = BeautifulSoup(page)
+    all_anchors = soup.find_all('a')
+    # print(soup)
+    print(all_anchors.__len__())
 
-        # from this point, work flow is as follows :
-        # pick a paper. if it does not exists in current data (papersname), create a new papersInfo.
-        # else add an additional column in the papersInfo
-        allTables = profile_soup.find_all('table')
-        dataTable = allTables[1]
+    faculty_link = soup.select_one("a[href*=/citations?user]")
+    profile_link = base_link + faculty_link['href']
+    # print(profile_link)
+    profile_page = urlopen(profile_link + "")
+    profile_soup = BeautifulSoup(profile_page)
+    print("\n")
+    # print(profile_soup)
+    # file.write(str(profile_soup))
 
-        # print(dataTable)
-        print(type(dataTable))
-        body = dataTable.find_all('body')
-        print(body)
+    # from this point, work flow is as follows :
+    # pick a paper. if it does not exists in current data (papersname), create a new papersInfo.
+    # else add an additional column in the papersInfo
 
-
+    allTables = profile_soup.find_all('table')
+    dataTable = allTables[1]
+    body = dataTable.find_all('tbody')
+    rows = body[0].find_all('tr')
+    columns = rows[0].find_all('td')
+    print(columns[0].find_all('a')[0].get_text())
+    print(columns[1].get_text())
+    print(columns[2].get_text())
 
 if __name__ == "__main__":
     main()
