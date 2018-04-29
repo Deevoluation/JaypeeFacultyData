eccda2e6e363ef6fb74b7e7728411d50f7076eae
DravitLochan <dlochangupta@gmail.com>
2018-04-29 23:33:12 +0530

    gile formatting

--- GoogleScholarScraper.py
+++ GoogleScholarScraper.py
@@ -4,6 +4,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup, NavigableString
 
 url = "https://scholar.google.co.in/citations?view_op=search_authors&mauthors=anuja+arora+jiit&hl=en&oi=ao"
+further_link = "&cstart=20&pagesize=80"
 base_link = "https://scholar.google.co.in"
 papersInfo = []
 papersname = set([])
@@ -47,6 +48,32 @@ def main():
             papersInfo.append(paper)
 
     print(papersInfo.__len__())
+
+    further_page = urlopen(profile_link + further_link)
+    further_soup = BeautifulSoup(further_page)
+
+    if "There are no articles in this profile." in str(further_soup):
+        print("next professor")
+        # meaning that only 1 page was there for the current faculty. Move to next faculty
+    else:
+        allTables = profile_soup.find_all('table')
+        dataTable = allTables[1]
+        body = dataTable.find_all('tbody')
+        rows = body[0].find_all('tr')
+        for row in rows:
+            columns = row.find_all('td')
+            # print(columns[0].find_all('a')[0].get_text())
+            # print(columns[1].get_text())
+            # print(columns[2].get_text())
+            if columns[0].find_all('a')[0].get_text() in papersname:
+                pos = papersname.index(columns[0].find_all('a'[0]))
+                papersInfo[pos].append("add name of current professor as a co-author to this paper")
+            else:
+                paper = [columns[0].find_all('a')[0].get_text(), ]
+                papersInfo.append(paper)
+
+    print(papersInfo.__len__())
+
 
 if __name__ == "__main__":
     main()
